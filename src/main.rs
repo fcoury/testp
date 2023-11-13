41b896d1930d7a4e2c5aeb2868f4d12e27d21b64
Felipe Coury <felipe.coury@gmail.com>
2023-11-13 11:10:16 -0300

    feat: works also as a library

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,11 @@
-use std::{
-    net::TcpListener,
-    sync::{mpsc, Arc},
-    thread,
-};
-
-use broadcaster::broadcaster;
 use clap::Parser;
 use cli::Cli;
-use client::client;
-use error::{Error, Result};
-use server::server;
-pub use server::Message;
-use target::Target;
+use testp::{start_proxy, Result};
 
-mod broadcaster;
 mod cli;
-mod client;
-mod error;
-mod server;
-mod target;
 
-/// A sketch of the new architecture for the yprox multiplexing, modifying proxy server
-///
-/// The terminology I'm using here is:
-///
-/// Client - a client process that would connect to the application we're multiplexing
-/// Server - the proxy server, a man-in-the-middle between the client and the application
-/// Target - each server that acts as the application
-///
 fn main() -> Result<()> {
     let args = Cli::parse();
-
-    let listener = TcpListener::bind(args.from_addr)?;
-    let targets = args.to_addr;
-
-    // used to send messages to the server
-    let (send_message, receive_message) = mpsc::channel();
-
-    // used to send broadcasts to all targets
-    let (send_broadcast, receive_broadcast) = mpsc::channel();
-
-    // spawn the server thread (handles server -> client and server -> broadcast)
-    // handles messages between client and server, and sends broadcasts
-    thread::spawn(|| server(receive_message, send_broadcast));
-
-    // spawn the broadcasting thread (handles server -> targets and targets -> server)
-    // the breadcaster receives broadcast requests and sends them to all targets
-    // it also receives the send_message handle so that each target can send individual
-    // responses to the server
-    let send_message_clone = send_message.clone();
-    thread::spawn(|| {
-        broadcaster(targets, receive_broadcast, send_message_clone)
-            .map_err(|err| eprintln!("{:?}", err))
-    });
-
-    // spawn the client threads (handle client -> server)
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let stream = Arc::new(stream);
-                let send_message = send_message.clone();
-                thread::spawn(|| client(stream, send_message));
-            }
-            Err(err) => {
-                eprintln!("Error accepting connection: {}", err);
-            }
-        }
-    }
-
+    start_proxy(args.from_addr, args.to_addr)?;
     Ok(())
 }
